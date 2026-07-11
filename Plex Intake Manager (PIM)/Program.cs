9cff9bc7823bd757b3b54d48e8cd3840986b066d
Matt Rowe <59579714+Stormfather-SLC@users.noreply.github.com>
2026-07-11 15:45:10 -0600

    Register destination profile services

--- Plex Intake Manager (PIM)/Program.cs
+++ Plex Intake Manager (PIM)/Program.cs
@@ -7,39 +7,37 @@ using PIM.Infrastructure.Services;
 using PIM.Web.Services;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddScoped<IFileScanner, FileScanner>();
-// Add services to the container.
+
 builder.Services.AddRazorPages();
+builder.Services.AddMemoryCache();
+
+builder.Services.AddScoped<IFileScanner, FileScanner>();
 builder.Services.AddScoped<IFileNameParser, FileNameParser>();
 builder.Services.AddHttpClient<IMetadataService, OmdbMetadataService>();
 builder.Services.AddScoped<IDuplicateService, DuplicateService>();
-builder.Services.AddScoped<IRenameService, RenameService>();
-builder.Services.AddMemoryCache();
-builder.Services.AddSingleton<ScanProgress>();
-builder.Services.AddScoped<PreviewTreeService>();
-builder.Services.AddScoped<IDryRunPreviewService, DryRunPreviewService>();
+builder.Services.AddScoped<IDestinationPathBuilder, DestinationPathBuilder>();
 builder.Services.AddScoped<IDestinationConflictService, DestinationConflictService>();
 builder.Services.AddScoped<IPlexLibraryConflictService, PlexLibraryConflictService>();
 builder.Services.AddScoped<IMovieConflictDetectionService, MovieConflictDetectionService>();
+builder.Services.AddScoped<IRenameService, RenameService>();
+
+builder.Services.AddSingleton<IDestinationProfileStore, DestinationProfileStore>();
+builder.Services.AddSingleton<ScanProgress>();
+builder.Services.AddScoped<PreviewTreeService>();
+builder.Services.AddScoped<IDryRunPreviewService, DryRunPreviewService>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
-
 app.UseStaticFiles();
-
 app.UseRouting();
-
 app.UseAuthorization();
-
 app.MapRazorPages();
 
 app.Run();
